e46aa4f36e2bcfbeec6e79aff1c605a3b5a9fe1d
luukvlug1905 <62696310+luukvlug1905@users.noreply.github.com>
2022-11-01 12:17:36 +0100

    Update app.py

--- streamlit/app.py
+++ streamlit/app.py
@@ -12,6 +12,8 @@ from streamlit_folium import folium_static
 st.set_page_config(
     layout="wide")
 
+st.title("Dashboard misdrijven in Amsterdam in de periode van 2013 t/m 2021")
+
 #politie df inladen
 df_politie = pd.read_csv('politie.csv')
 
